1eeea9e676e750466340205bebb4f46d580b311e
Neil Warrack <neil.warrack@gmail.com>
2018-07-07 14:15:30 +0100

    working on vetoing dressed leptons

--- ATLAS_2017_I1512776/ATLAS_2017_I1512776.cc
+++ ATLAS_2017_I1512776/ATLAS_2017_I1512776.cc
@@ -57,13 +57,13 @@ namespace Rivet {
       IdentifiedFinalState electron_fs;
       electron_fs.acceptIdPair(PID::ELECTRON);
       PromptFinalState promptElectrons(electron_fs, true, false);
-      DressedLeptons dressedElectrons(photons, promptElectrons, 0.1, e_muCuts, false);
+      DressedLeptons dressedElectrons(photons, promptElectrons, 0.1, e_muCuts, true);
       declare(dressedElectrons, "DressedElectrons");
       
       IdentifiedFinalState muon_fs;
       muon_fs.acceptIdPair(PID::MUON);
-      PromptFinalState promptMuons(muon_fs, true, false);
-      DressedLeptons dressedMuons(photons, promptMuons, 0.1, e_muCuts, false);
+      PromptFinalState promptMuons(muon_fs, true, false); //< "true, false" accepts tau decays but not muon decays
+      DressedLeptons dressedMuons(photons, promptMuons, 0.1, e_muCuts, true); //< last arg includes non-prompt photons
       declare(dressedMuons, "DressedMuons");
       
       WFinder w_electron(electron_fs, e_muCuts,PID::ELECTRON,35*GeV, 8000*GeV, 30*GeV, 0.1,  WFinder::PROMPTCHLEPTONS, WFinder::CLUSTERNODECAY,WFinder::TRACK,  WFinder::TRANSMASS);
@@ -72,13 +72,15 @@ namespace Rivet {
       WFinder w_muon(muon_fs,  e_muCuts, PID::MUON, 35*GeV, 100*GeV, 30*GeV, 0.1, WFinder::PROMPTCHLEPTONS, WFinder::CLUSTERNODECAY, WFinder::TRACK, WFinder::TRANSMASS);
       declare(w_muon, "W_Muon");
       
-      VetoedFinalState jet_input(fs);
-      jet_input.addVetoOnThisFinalState(dressedElectrons);
-      jet_input.addVetoOnThisFinalState(dressedMuons);
+      VetoedFinalState jet_input();//< the "selected lepton (with it's photons) is not to be included in jet clustering
+      jet_input.addVetoOnThisFinalState(dressedElectrons->bareLepton());
+      jet_input.addVetoOnThisFinalState(dressedElectrons->photons());
+      //jet_input.addVetoOnThisFinalState(dressedMuons);
       declare(FastJets(jet_input, FastJets::ANTIKT, 0.4), "Jets");
-      
-      declare(PartonicTops(PartonicTops::E_MU), "LeptonicTops") ;
-      declare(PartonicTops(PartonicTops::ALL ), "AllPartonicTops") ;
+
+      // do parton level analysis seperatelty!!
+      //declare(PartonicTops(PartonicTops::E_MU), "LeptonicTops") ;
+      //declare(PartonicTops(PartonicTops::ALL ), "AllPartonicTops") ;
       
       
       // book histograms
@@ -116,18 +118,18 @@ namespace Rivet {
       const WFinder& w_mu = apply<WFinder>(event, "W_Muon");
       const Particles w_elP= w_el.bosons();
       const Particles w_muP= w_mu.bosons();
-      const Particles leptonicTops = apply<ParticleFinder>(event, "LeptonicTops").particlesByPt();
-      const Particles allPartonicTops = apply<ParticleFinder>(event, "AllPartonicTops").particlesByPt();
+      //const Particles leptonicTops = apply<ParticleFinder>(event, "LeptonicTops").particlesByPt();
+      //const Particles allPartonicTops = apply<ParticleFinder>(event, "AllPartonicTops").particlesByPt();
 
       /// Particle-level analysis
       
       bool particleVeto = false;
 
       // find jets
-      Cut jetCuts = Cuts::abseta < 4.5 && Cuts::pT > 30*GeV ;
-      cout << "sizeOfJets1=" << jets.size()<< " "; RET;
+      Cut jetCuts = (Cuts::abseta < 4.5 && Cuts::pT > 30*GeV);
+      cout << "sizeOfJetsPreFilter=" << jets.size()<< " "; RET;
       ifilter_select(jets, jetCuts);
-      cout << "sizeOfJets2=" << jets.size()<< " "; RET;
+      cout << "sizeOfJetsPostFilter=" << jets.size()<< " "; RET;
 
       // Find the leptons
       cout << "muons.size()=" << muons.size(); RET;
@@ -165,26 +167,31 @@ namespace Rivet {
         }
       }
 
-      // veto at particle level
+
+      
+      // veto at particle level**************************************************
       if(leptons.size() != 1) {N2;RET; vetoEvent;}
       else if(b!=1) {N3;RET;vetoEvent;}
       else if(jets.size() != 2) {N4;RET;vetoEvent;}
       else if(particleVeto) {N5;RET;vetoEvent;} // lepton too close to a jet
+      else {N6;}//success!
+
+      // Then this implies there must be 2 jets of which only one is
+      // b-tagged and there is exactly one isolated particle level
+      // electron or muon in the event.
+      //*************************************************************************
 
 
-      if ( b==1 && leptons.size() == 1 && jets.size() == 2 && !particleVeto ){
 
-	// Then this implies there must be 2 jets of which only one is
-        // b-tagged and there is exactly one isolate particle level
-        // electron or muon in the event.
-        // make cut on the mass of lepton + b-jet system
-        FourMomentum lepton4p = leptons[0];
-        FourMomentum bJet4p = bJet;
-        FourMomentum lb4p = lepton4p + bJet4p;
+      
+      
+      // make cut on the mass of lepton + b-jet system
+      FourMomentum lepton4p = leptons[0];
+      FourMomentum bJet4p = bJet;
+      FourMomentum lb4p = lepton4p + bJet4p;
+      if ( lb4p.mass() < 160*GeV ) {N7;RET;vetoEvent;}
 
-	if ( lb4p.mass() < 160*GeV ) particleVeto = true;
-        if(particleVeto) {N6;RET;vetoEvent;}
-  
+      
       FourMomentum w4p;
       // cout << "W-el=" << w_elP.size() << ", " ;
       // cout << "W-mu=" << w_muP.size() << " " ;
@@ -195,41 +202,42 @@ namespace Rivet {
       if ( w_elP.size() + w_muP.size() == 1){
 	if   ( w_elP.size() == 1 )    w4p = w_elP[0] ;
 	else if ( w_muP.size() == 1 ) w4p = w_muP[0] ;
-	else {particleVeto = true;}
-	
-	if (!particleVeto){
-	  
-	  // define pseudoTop
-	  const FourMomentum pseudoTop4p = bJet4p + w4p;
-	  
-	  if (leptons[0].charge() > 0){
-	    N7;
-	    _c_fid_t->fill(event.weight()) ;
-	    
-	    _h_AbsPtclDiffXsecTPt->fill(pseudoTop4p.pT(),     event.weight()) ;
-	    _h_AbsPtclDiffXsecTPt->fill(pseudoTop4p.absrap(), event.weight()) ;
-	    
-	    _h_NrmPtclDiffXsecTPt->fill(pseudoTop4p.pT(),     event.weight()) ;
-	    _h_NrmPtclDiffXsecTPt->fill(pseudoTop4p.absrap(), event.weight()) ;
-	    
-	    
-	  } else {
-	    N8;
-	    _c_fid_tbar->fill(event.weight()) ;
-	    
-	    _h_AbsPtclDiffXsecTbarPt->fill(pseudoTop4p.pT(),     event.weight()) ;
-	    _h_AbsPtclDiffXsecTbarPt->fill(pseudoTop4p.absrap(), event.weight()) ;
-	    
-	    _h_NrmPtclDiffXsecTbarPt->fill(pseudoTop4p.pT(),     event.weight()) ;
-	    _h_NrmPtclDiffXsecTbarPt->fill(pseudoTop4p.absrap(), event.weight()) ;
-	    
-	  }
-	}
+	else {WORRY;}
       }
-      else if ( w_elP.size() + w_muP.size() > 1) {N1;}
-      }//< end of particle-level analysis
-    cout << "particleVeto=" << particleVeto;RET;
+
+	  
+      // define pseudoTop
+      const FourMomentum pseudoTop4p = bJet4p + w4p;
+      
+      if (leptons[0].charge() > 0){
+	N8;
+	_c_fid_t->fill(event.weight()) ;
+	
+	_h_AbsPtclDiffXsecTPt->fill(pseudoTop4p.pT(),     event.weight()) ;
+	_h_AbsPtclDiffXsecTPt->fill(pseudoTop4p.absrap(), event.weight()) ;
+	
+	_h_NrmPtclDiffXsecTPt->fill(pseudoTop4p.pT(),     event.weight()) ;
+	_h_NrmPtclDiffXsecTPt->fill(pseudoTop4p.absrap(), event.weight()) ;
+	
+	
+      } else {
+	N9;
+	_c_fid_tbar->fill(event.weight()) ;
+	
+	_h_AbsPtclDiffXsecTbarPt->fill(pseudoTop4p.pT(),     event.weight()) ;
+	_h_AbsPtclDiffXsecTbarPt->fill(pseudoTop4p.absrap(), event.weight()) ;
+	
+	_h_NrmPtclDiffXsecTbarPt->fill(pseudoTop4p.pT(),     event.weight()) ;
+	_h_NrmPtclDiffXsecTbarPt->fill(pseudoTop4p.absrap(), event.weight()) ;
+	
+      }
     
+  
+      //else if ( w_elP.size() + w_muP.size() > 1) {N1;}
+  //< end of particle-level analysis
+
+//cout << "particleVeto=" << particleVeto;RET;
+/*  
 
     /// Parton-level analysis
     bool partonVeto = false;
@@ -259,8 +267,8 @@ namespace Rivet {
     
     RET;
 
-
-    } //< end of analyze()            
+*/
+} //< end of analyze()            
 
    
 
