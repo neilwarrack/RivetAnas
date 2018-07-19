a092cf700d742859d405e12329567f8b04d4edd1
Neil Warrack <neil.warrack@gmail.com>
2018-07-19 18:17:00 +0100

    nvc

---
+++ CMS_2017_I1607560/rivet-mkanalysis/CMS_2017_I1607560.cc
@@ -0,0 +1,68 @@
+// -*- C++ -*-
+#include "Rivet/Analysis.hh"
+#include "Rivet/Projections/FinalState.hh"
+#include "Rivet/Projections/FastJets.hh"
+
+namespace Rivet {
+
+
+  /// @brief Add a short analysis description here
+  class CMS_2017_I1607560 : public Analysis {
+  public:
+
+    /// Constructor
+    DEFAULT_RIVET_ANALYSIS_CTOR(CMS_2017_I1607560);
+
+
+    /// @name Analysis methods
+    //@{
+
+    /// Book histograms and initialise projections before the run
+    void init() {
+
+      // Initialise and register projections
+      declare(FinalState(Cuts::abseta < 5 && Cuts::pT > 100*MeV), "FS");
+
+      // Book histograms
+      _h_XXXX = bookProfile1D(1, 1, 1);
+      _h_YYYY = bookHisto1D(2, 1, 1);
+      _h_ZZZZ = bookCounter(3, 1, 1);
+
+    }
+
+
+    /// Perform the per-event analysis
+    void analyze(const Event& event) {
+
+      /// @todo Do the event by event analysis here
+
+    }
+
+
+    /// Normalise histograms etc., after the run
+    void finalize() {
+
+      normalize(_h_YYYY); // normalize to unity
+      scale(_h_ZZZZ, crossSection()/picobarn/sumOfWeights()); // norm to cross section
+
+    }
+
+    //@}
+
+
+    /// @name Histograms
+    //@{
+    Profile1DPtr _h_XXXX;
+    Histo1DPtr _h_YYYY;
+    CounterPtr _h_ZZZZ;
+    //@}
+
+
+  };
+
+
+  // The hook for the plugin system
+  DECLARE_RIVET_PLUGIN(CMS_2017_I1607560);
+
+
+}
